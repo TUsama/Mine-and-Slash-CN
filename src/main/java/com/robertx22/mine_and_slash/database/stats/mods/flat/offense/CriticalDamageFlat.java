070ce8f22a9d2768d7cf81c2c787195f50b1e84c
mahjerion <66142256+mahjerion@users.noreply.github.com>
2021-01-06 13:20:02 -0500

    crit change

--- src/main/java/com/robertx22/mine_and_slash/database/stats/mods/flat/offense/CriticalDamageFlat.java
+++ src/main/java/com/robertx22/mine_and_slash/database/stats/mods/flat/offense/CriticalDamageFlat.java
@@ -9,12 +9,12 @@ public class CriticalDamageFlat extends StatMod {
 
     @Override
     public float Min() {
-        return 2;
+        return 3;
     }
 
     @Override
     public float Max() {
-        return 6;
+        return 8;
     }
 
     @Override
