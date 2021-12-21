52a6dc85d1b222ae74d12588db8b6abf16b61d68
mahjerion <66142256+mahjerion@users.noreply.github.com>
2021-12-21 01:05:11 -0500

    fix dodge

--- src/main/java/com/robertx22/mine_and_slash/database/map_affixes/beneficial/BonusDodgeAffix.java
+++ src/main/java/com/robertx22/mine_and_slash/database/map_affixes/beneficial/BonusDodgeAffix.java
@@ -3,6 +3,7 @@ package com.robertx22.mine_and_slash.database.map_affixes.beneficial;
 import com.robertx22.mine_and_slash.database.map_affixes.BeneficialMapAffix;
 import com.robertx22.mine_and_slash.database.stats.StatMod;
 import com.robertx22.mine_and_slash.database.stats.mods.flat.defense.ArmorFlat;
+import com.robertx22.mine_and_slash.database.stats.mods.flat.defense.DodgeRatingFlat;
 import com.robertx22.mine_and_slash.saveclasses.gearitem.StatModData;
 
 import java.util.Arrays;
@@ -17,7 +18,7 @@ public class BonusDodgeAffix extends BeneficialMapAffix {
 
     @Override
     public List<StatModData> Stats(int percent) {
-        return Arrays.asList(StatModData.Load(new ArmorFlat().size(StatMod.Size.DOUBLE), percent));
+        return Arrays.asList(StatModData.Load(new DodgeRatingFlat().size(StatMod.Size.DOUBLE), percent));
     }
 
     @Override
