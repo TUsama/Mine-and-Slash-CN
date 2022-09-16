4bdd36b871d3010b82858e86cb86e1fe7e27a10a
mahjerion <66142256+mahjerion@users.noreply.github.com>
2022-09-16 12:40:34 -0400

    UPDATE TEAM BONUS

--- src/main/java/com/robertx22/mine_and_slash/database/map_affixes/beneficial/TeamBonusAffix.java
+++ src/main/java/com/robertx22/mine_and_slash/database/map_affixes/beneficial/TeamBonusAffix.java
@@ -30,8 +30,8 @@ public class TeamBonusAffix extends BeneficialMapAffix {
 
     @Override
     public List<StatModData> Stats(int percent) {
-        return Arrays.asList(StatModData.Load(new HealthMulti().size(StatMod.Size.FIFTY), percent),
-                StatModData.Load(new DamageShieldFlat().size(StatMod.Size.TEN), percent),
+        return Arrays.asList(StatModData.Load(new HealthMulti().size(StatMod.Size.FIFTY), 100),
+                StatModData.Load(new DamageShieldFlat().size(StatMod.Size.TEN), 100),
                 StatModData.Load(new IncreaseDamageFlat().size(StatMod.Size.TENTEN), percent),
                 StatModData.Load(new SpeedyFlat(), 100));
     }
